c0c462599884b9b009b0a929bde16ad9f242cdb2
roshankaranth <roshankaranth@gmail.com>
2025-04-13 07:48:45 +0530

    task 1

---
+++ Lab 11/heap.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct heap{
+    int *data;
+    int size;
+    int capacity;
+    int depth;
+}Heap;
+
+Heap* heap_create(){
+    Heap* h = (Heap*)malloc(sizeof(Heap));
+
+    h->data = (int*)malloc(sizeof(int));
+    h->size = 0;
+    h->capacity = 1;
+    h->depth = 0;
+
+    return h;
+}
+
+int power(int a, int n){
+    int res = 1;
+    for(int i = 0 ; i < n ; i++){
+        res *= a;
+    }
+
+    return res;
+}
+
+void add_to_tree(Heap* h, int val){
+    
+    if(h->capacity == h->size){
+        h->depth++;
+        h->data = (int*)realloc(h->data,sizeof(Heap)*(power(2,h->depth+1) -1));
+        h->capacity = power(2,h->depth+1)-1;
+
+    }
+
+    h->data[h->size] = val;
+    h->size++;
+
+}
+
+void display(Heap* h){
+    for(int i = 0 ; i < h->size ; i++){
+        printf("%d ",h->data[i]);
+    }
+    printf("\n");
+}
+
+void main(){
+    Heap* h = heap_create();
+
+    add_to_tree(h,1);
+    add_to_tree(h,4);
+    add_to_tree(h,2);
+    add_to_tree(h,3);
+    add_to_tree(h,9);
+    add_to_tree(h,7);
+    add_to_tree(h,8);
+    add_to_tree(h,10);
+    add_to_tree(h,14);
+    add_to_tree(h,16);
+
+    display(h);
+
+    return;
+}
